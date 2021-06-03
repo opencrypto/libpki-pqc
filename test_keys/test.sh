b90fed0402706f8c6b37366c5791c056a15d08da
Massimiliano Pala <m.pala@cablelabs.com>
2021-06-03 02:04:48 -0600

    Added example keys and extended test key generator's script.

--- test_keys/test.sh
+++ test_keys/test.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Generates some classic keys
+pki-tool genkey -algor ec -bits 256 -out ec.key
+pki-tool genkey -algor rsa -bits 2048 -out rsa.key
+ 
+# Generates some Post-Quantum keys
+pki-tool genkey -algor falcon -bits 128 -out falcon.key
+pki-tool genkey -algor dilithium -bits 128 -out dilithium.key
+
+# Generates a Classic Composite
+pki-tool genkey -algor composite -addkey rsa.key -addkey test_keys/ec.key
+
+# Generates a Post-Quantum Composite
+pki-tool genkey -algor composite -addkey falcon.key -addkey test_keys/dilithium.key
+
+# Generates an Hybrid key (i.e., a mix of Classic and Post-Quantum algorithms)
 pki-tool genkey -debug -verbose -algor composite -addkey falcon.key -addkey ec.key -addkey dilithium.key -addkey rsa.key -out composite.key
 
 exit 0
