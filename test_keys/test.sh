8a29acc2a555bc6eae369a2a627c3380738a852d
Massimiliano Pala <director@openca.org>
2021-06-03 23:11:42 -0600

    Updated patch for composite crypto support.

--- test_keys/test.sh
+++ test_keys/test.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 # Generates some classic keys
-pki-tool genkey -algor ec -bits 256 -out ec.key
-pki-tool genkey -algor rsa -bits 2048 -out rsa.key
+pki-tool genkey -batch -algor ec -bits 256 -out ec.key
+pki-tool genkey -batch -algor rsa -bits 2048 -out rsa.key
  
 # Generates some Post-Quantum keys
-pki-tool genkey -algor falcon -bits 128 -out falcon.key
-pki-tool genkey -algor dilithium -bits 128 -out dilithium.key
+pki-tool genkey -batch -algor falcon -bits 128 -out falcon.key
+pki-tool genkey -batch -algor dilithium -bits 128 -out dilithium.key
 
 # Generates a Classic Composite
-pki-tool genkey -algor composite -addkey rsa.key -addkey test_keys/ec.key
+pki-tool genkey -batch -algor composite -addkey rsa.key -addkey ec.key
 
 # Generates a Post-Quantum Composite
-pki-tool genkey -algor composite -addkey falcon.key -addkey test_keys/dilithium.key
+pki-tool genkey -batch -algor composite \
+	-addkey falcon.key       \
+	-addkey dilithium.key
 
 # Generates an Hybrid key (i.e., a mix of Classic and Post-Quantum algorithms)
-pki-tool genkey -debug -verbose -algor composite -addkey falcon.key -addkey ec.key -addkey dilithium.key -addkey rsa.key -out composite.key
+pki-tool genkey -batch -algor composite \
+	-addkey falcon.key \
+	-addkey ec.key \
+	-addkey dilithium.key \
+	-addkey rsa.key \
+	-out composite.key
 
 exit 0
 
