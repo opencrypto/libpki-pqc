1235bdfe2de2f1182bab7ad8907c30cce129cc83
Massimiliano Pala <director@openca.org>
2021-06-13 22:45:41 -0600

    First version with positive verify for both PQ + No-Digest and PQ + Digest signing schemes. Hybrid is supported but not tested.

--- test_keys/ossl-req-test.sh
+++ test_keys/ossl-req-test.sh
@@ -6,12 +6,16 @@ openssl req -new -key ec.key -subj "/C=US/OU=EC-P256" -out ossl-ec.req
  
 # Generates some Post-Quantum keys
 openssl req -new -key falcon.key -subj "/C=US/OU=Falcon512" -out ossl-falcon512.req
+openssl req -sha384 -new -key falcon.key -subj "/C=US/OU=Falcon512" -out ossl-falcon512-sha384.req
 openssl req -new -key dilithium.key -subj "/C=US/OU=Dilithium2" -out ossl-dilithium2.req
+openssl req -sha384 -new -key dilithium.key -subj "/C=US/OU=Dilithium2" -out ossl-dilithium2-sha384.req
 
 # Generates a Classic Composite
 # No Equivalent in OpenSSL
 openssl req -new -key composite.key -subj "/C=US/OU=Composite One" -out ossl-composite-one.req
+openssl req -sha384 -new -key composite.key -subj "/C=US/OU=Composite One" -out ossl-composite-one.req
 openssl req -new -key composite.key -subj "/C=US/OU=Composite Two" -out ossl-composite-two.req
+openssl req -sha384 -new -key composite.key -subj "/C=US/OU=Composite Two" -out ossl-composite-two.req
 
 exit 0
 
